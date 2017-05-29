ed286038d504b438b19029b6c80ecf3dcb308ac5
Vera <vera.low.2015@sis.smu.edu.sg>
2017-05-29 15:30:38 +0800

    ajax nt working

--- RSVP/RSVP/Controllers/ShowMapController.cs
+++ RSVP/RSVP/Controllers/ShowMapController.cs
@@ -19,6 +19,29 @@ namespace RSVP.Controllers
             return View();
         }
 
-
+        public ActionResult HandleChecks(String check)
+        {
+            var userOptions = check.Split('|').ToArray();
+            LukeRefL2.L2 lukeL2Obj = new LukeRefL2.L2();
+            double[] driverLat = new double[200];
+            double[] driverLong = new double[200];
+            for (int i =0; i<userOptions.Length; i++)
+            {
+                if (userOptions[i].Contains("#"))
+                {
+                    long driverID = long.Parse(userOptions[i].Substring(1));
+                    var driverLoc = lukeL2Obj.GetLocations("130FEE3E0ACA2B608929CE0DEA1C15812365AAE6",driverID,30);
+                    for(int j =0; j< driverLoc.Length; j++)
+                    {
+                        driverLat[j] = driverLoc[j].Latitude;
+                        driverLong[j] = driverLoc[j].Longitude;
+                    }
+                }
+            }
+            ViewBag.Lat = driverLat;
+            ViewBag.Long = driverLong;
+            ViewBag.NumLoc = driverLat.Length;
+            return View();
+        }
     }
 }
