72e45640b7a2eb6bc10a66675f14b4bedc0b60d6
Vera <vera.low.2015@sis.smu.edu.sg>
2017-06-01 16:48:26 +0800

    try

--- RSVP/RSVP/Controllers/ShowMapController.cs
+++ RSVP/RSVP/Controllers/ShowMapController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,33 +16,46 @@ namespace RSVP.Controllers
             LukeRefL2.L2 lukeL2Obj = new LukeRefL2.L2();
             Array driverArr = lukeL2Obj.GetL2Drivers("130FEE3E0ACA2B608929CE0DEA1C15812365AAE6");
             ViewBag.Size = driverArr.Length;
-            ViewBag.Collection = driverArr;
+            ViewBag.driverArr = driverArr;
             return View();
         }
 
-        public ActionResult HandleChecks(String check)
+        public ActionResult HandleDriverOptions(String check)
         {
             var userOptions = check.Split('|').ToArray();
-            LukeRefL2.L2 lukeL2Obj = new LukeRefL2.L2();
-            double[] driverLat = new double[200];
-            double[] driverLong = new double[200];
+            LukeRefL2.L2 lukeL2Obj = new LukeRefL2.L2();            
+            ArrayList allDriverLatLong = new ArrayList();
             for (int i =0; i<userOptions.Length; i++)
             {
-                if (userOptions[i].Contains("#"))
+                double[] driverLat = new double[5];
+                double[] driverLong = new double[5];
+                if (userOptions[i].Contains("#")) //means a driver
                 {
                     long driverID = long.Parse(userOptions[i].Substring(1));
-                    var driverLoc = lukeL2Obj.GetLocations("130FEE3E0ACA2B608929CE0DEA1C15812365AAE6",driverID,30);
+                    var driverLoc = lukeL2Obj.GetLocations("130FEE3E0ACA2B608929CE0DEA1C15812365AAE6",driverID,5);
                     for(int j =0; j< driverLoc.Length; j++)
                     {
-                        driverLat[j] = driverLoc[j].Latitude;
-                        driverLong[j] = driverLoc[j].Longitude;
+                        driverLat[j] = driverLoc[j].Latitude; //30 of the driver's location
+                        driverLong[j] = driverLoc[j].Longitude; 
                     }
+                    ArrayList driverLatLong = new ArrayList();
+                    driverLatLong.Add(driverLat); 
+                    driverLatLong.Add(driverLong);
+                    allDriverLatLong.Add(driverLatLong);
                 }
             }
-            ViewBag.Lat = driverLat;
-            ViewBag.Long = driverLong;
-            ViewBag.NumLoc = driverLat.Length;
-            return View();
+            //ViewBag.AllDriverLoc = allDriverLatLong;
+            return Json(allDriverLatLong,JsonRequestBehavior.AllowGet);
+            //return PartialView();
+
         }
+
+
+        public ActionResult Test(String test)
+        {
+            return Content("Test Success", "text/plain");
+            
+        }
+
     }
 }
---
+++ Try/Try/ShowMap.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Try
+{
+    public partial class ShowMap : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+           if (!IsPostBack)
+            {
+                
+
+
+                //load your check box list
+                var driverArray = getDriverArray();
+                for(int i = 0; i < driverArray.Length; i++) {
+
+                }
+
+            } else
+            {
+          
+                                
+                 
+            }
+        }
+
+        public LukeRefL2.DriverObject[] getDriverArray()
+        {
+            LukeRefL2.L2 luke2Obj = new LukeRefL2.L2();
+
+            LukeRefL2.DriverObject[] arrayOfDrivers = luke2Obj.GetL2Drivers("130FEE3E0ACA2B608929CE0DEA1C15812365AAE6");
+            return arrayOfDrivers;
+
+        }
+
+
+    }
+}
